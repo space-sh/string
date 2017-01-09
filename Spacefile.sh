512c39a058a84a189d926499d8bd89264c2471d1
Thomas Backlund <blund@blockie.org>
2017-01-09 14:37:25 +0100

    Initial

---
+++ Spacefile.sh
@@ -0,0 +1,37 @@
+#
+# Copyright 2017 Blockie AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#============
+# STRING_TRIM
+#
+# Trim whitespace from a string left and right.
+#
+# Parameters:
+#   $1: the name of the variable to trim.
+#
+# Expects:
+#   $1: The variable should be set with the value to trim.
+#       This variable is directly trimmed on.
+#
+# Returns:
+#   non-zero if string error
+#
+#============
+STRING_TRIM()
+{
+    eval "${1}=\"\${$1#\"\${$1%%[^ ]*}\"}\""
+    eval "${1}=\"\${$1%\"\${$1##*[^ ]}\"}\""
+}
